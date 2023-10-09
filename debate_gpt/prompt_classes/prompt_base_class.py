842b1ef25dfe1366396f2a615edf4e23ecd196ae
Paula Rescala <paula.rescala@epfl.ch>
2023-10-09 14:16:09 +0200

    Add base class for prompting chatgpt.

---
+++ debate_gpt/prompt_classes/prompt_base_class.py
@@ -0,0 +1,67 @@
+import os
+from typing import Optional
+
+import openai
+import tiktoken
+
+
+class PromptBaseClass:
+    def __init__(
+        self,
+        max_tokens: int = 4097,
+        model: str = "gpt-3.5-turbo",
+    ) -> None:
+        # set model info
+        self._max_tokens = max_tokens
+        self._model = model
+        self._encoding = tiktoken.encoding_for_model(model)
+
+        # set api key
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    @property
+    def max_tokens(self):
+        return self._max_tokens
+
+    @staticmethod
+    def get_user_message(message: str) -> dict[str, str]:
+        return {"role": "user", "content": message}
+
+    @staticmethod
+    def get_system_message(message: str) -> dict[str, str]:
+        return {"role": "system", "content": message}
+
+    @staticmethod
+    def get_assistant_message(message: str) -> dict[str, str]:
+        return {"role": "assistant", "content": message}
+
+    def count_tokens(self, message: str) -> int:
+        """Return the number of tokens in `message` according to the encoding for the
+        OpenAI model being used.
+        """
+        return len(self._encoding.encode(message))
+
+    def prompt_chat_gpt(
+        self, messages: list[str], max_tokens: Optional[int] = 16
+    ) -> openai.openai_object.OpenAIObject:
+        return openai.ChatCompletion.create(
+            model=self._model, messages=messages, max_tokens=max_tokens
+        )
+
+    def calculate_cost_input(self, num_tokens: int) -> float:
+        """Return the cost of inputting `num_tokens` into the model."""
+        if self._model == "gpt-3.5-turbo":
+            cost = (num_tokens * 0.0015) / 1000
+        else:
+            raise ValueError(f"Model {self._model} cost unknown.")
+
+        return cost
+
+    def calculate_cost_output(self, num_tokens: int) -> float:
+        """Return the cost of outputting `num_tokens` from the model."""
+        if self._model == "gpt-3.5-turbo":
+            cost = (num_tokens * 0.002) / 1000
+        else:
+            raise ValueError(f"Model {self._model} cost unknown.")
+
+        return cost
